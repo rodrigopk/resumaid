93edd5288e024636aa25b53e2ccd9d4ba92ad91e
rodrigopk <rodrigopk@gmail.com>
2019-02-24 10:50:08 +0100

    Fix spec

--- spec/web/controllers/home/index_spec.rb
+++ spec/web/controllers/home/index_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 RSpec.describe Web::Controllers::Home::Index, type: :action do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
